0c0d101c6ee07eef0626f99825988c35680a55b1
ashnamariyashaji <94775121+ashnamariyashaji@users.noreply.github.com>
2024-10-04 12:26:13 +0530

    Add files via upload

---
+++ CounterProject/CounterProject/CounterConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CounterProject
+{
+    static class CounterConfig
+    {
+        public static Window FrmCounterWindow { get; set; }
+        public static CounterViewModel VueModel { get; set; }
+
+        static CounterConfig()
+        {
+            VueModel = new CounterViewModel();
+            FrmCounterWindow = new CounterWindow();
+        }
+    }
+}
---
+++ CounterProject/CounterProject/CounterViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CounterProject
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private Counter _newCounter = null;
+        public Counter NewCounter
+        {
+            get => _newCounter;
+            set { _newCounter = value; OnPropertyChanged(nameof(NewCounter)); }
+        }
+
+        public ICommand PlusCommand { get; }
+        public ICommand MinusCommand { get; }
+
+        public CounterViewModel()
+        {
+            this.NewCounter = new Counter { Number = 0 };
+
+            PlusCommand = new RelayCommand(Increament);
+            MinusCommand = new RelayCommand(Decreament);
+        }
+
+        public void Increament()
+        {
+
+            NewCounter.Number++;
+            OnPropertyChanged(nameof(NewCounter));
+
+        }
+        public void Decreament()
+        {
+
+            NewCounter.Number--;
+            OnPropertyChanged(nameof(NewCounter));
+        }
+    }
+}

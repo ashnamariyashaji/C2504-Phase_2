82242853a636f79044c3d7ec33847c5cc745da61
ashnamariyashaji <94775121+ashnamariyashaji@users.noreply.github.com>
2024-10-04 12:22:10 +0530

    Add files via upload

---
+++ FullNameApp1/FullNameApp1/EditFullNameWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FullNameApp1
+{
+    /// <summary>
+    /// Interaction logic for EditFullNameWindow.xaml
+    /// </summary>
+    public partial class EditFullNameWindow : Window
+    {
+        public EditFullNameWindow()
+        {
+            InitializeComponent();
+            this.DataContext = FullNameConfig.VueModel;
+        }
+        public void WindowClose()
+        {
+            this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ FullNameApp1/FullNameApp1/PersonViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace FullNameApp1
+{
+    public delegate void DWidnowClose();
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public DWidnowClose NewWindowClose;
+        public DWidnowClose EditWindowClose;
+        //
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        //        
+        private PersonModel _person = null;
+        public PersonModel Person
+        {
+            get => _person;
+            set { _person = value; OnPropertyChanged(nameof(Person)); }
+        }
+        //
+        private PersonModel _editablePerson = null;
+        public PersonModel EditablePerson
+        {
+            get => _editablePerson;
+            set { _editablePerson = value; OnPropertyChanged(nameof(EditablePerson)); }
+        }
+
+        
+        public ICommand UpdateCommand { get; }
+     
+        //
+        public PersonViewModel()
+        {
+            this.Person = new PersonModel { FirstName = "Ashna", LastName = "Mariya" };
+            this.EditablePerson = new PersonModel();
+            UpdateCommand = new RelayCommand(Update);
+
+        }
+        public void Update()
+        {
+            if (this.EditablePerson == null)
+            {
+                return;
+            }
+            this.Person.FirstName = this.EditablePerson.FirstName;
+            this.Person.LastName = this.EditablePerson.LastName;
+            this.Person = this.Person;
+            var result = MessageBox.Show(messageBoxText: "Updated Successfully",
+                    caption: "Alert",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Information);
+            if (EditWindowClose != null)
+            {
+                EditWindowClose();
+            }
+        }
+    }
+        }
+
+    

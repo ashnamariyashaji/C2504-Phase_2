98a7ab4d2d19737612039820442f1a90d7bff2fe
ashnamariyashaji <94775121+ashnamariyashaji@users.noreply.github.com>
2024-10-04 12:34:26 +0530

    Add files via upload

---
+++ Example13a/MenuWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13a
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        private MainWindow frmmainwindow =new MainWindow();
+        private SumCalcWindow frmcalcWindow =new SumCalcWindow();
+        private SquareCalcWindow1 frmsquarecalcWindow1 =new SquareCalcWindow1();
+        private void btnCounter_Click(object sender, RoutedEventArgs e)
+        {
+            frmmainwindow.Show();
+            frmmainwindow.Activate();
+        }
+
+        private void btnSumCalc_Click(object sender, RoutedEventArgs e)
+        {
+           frmcalcWindow.Show();
+            frmcalcWindow.Activate();
+        }
+
+        private void btnSquareCubeCalc_Click(object sender, RoutedEventArgs e)
+        {
+            frmsquarecalcWindow1.Show();
+            frmsquarecalcWindow1.Activate();
+        }
+    }
+}
---
+++ Example13a/SquareCalcWindow1.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13a
+{
+    /// <summary>
+    /// Interaction logic for SquareCalcWindow1.xaml
+    /// </summary>
+    public partial class SquareCalcWindow1 : Window
+    {
+        public SquareCalcWindow1()
+        {
+            InitializeComponent();
+        }
+        private bool IsNum(string numStr)
+        {
+            try
+            {
+                int.Parse(numStr);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter the number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+            int number = int.Parse(txtNumber.Text);
+            int square = (int)Math.Pow(number, 2);
+            txtResult.Text = square.ToString();
+            lblResult.Text = "Square";
+        }
+
+        private void btnCube_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter the number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+            int number=int.Parse(txtNumber.Text);
+            int cube = number * number * number;
+            txtResult.Text = cube.ToString();
+            lblResult.Text = "Cube";
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
